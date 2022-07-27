f052d6af901deb94e164069e4048ae6139be8c59
tiennguyenskedulo <tien.nguyen@skedulo.com>
2022-07-27 20:52:08 +0700

    Initial Commit

---
+++ TestingGit/TestingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGitApp.swift
+//  TestingGit
+//
+//  Created by Tien Nguyen on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
